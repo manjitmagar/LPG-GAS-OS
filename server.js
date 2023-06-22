a3b84c5f9ed532bbf3103774dc6b3a2348822660
manjitmagar <manjit.191521@ncit.edu.np>
2023-06-22 11:23:35 +0545

    added

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const path =require('path')
+const ejs =require('ejs')
+const expressLayout =require('express-ejs-layouts')
+const PORT = process.env.PORT|| 3000 
+
+//Assets
+app.use(express.static('public'))
+
+
+ app.get('/',(req,res) => {
+       res.render('home')
+ })
+
+ //set Template engine 
+ app.use(expressLayout)
+ app.set('views',path.join(__dirname,'/resources/views'))
+ app.set('view engine' ,'ejs')
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ok  ${PORT}`)
+    })
