2cd89ecc5922c338912fffbfc18127d08e9c6d6f
manjitmagar <manjit.191521@ncit.edu.np>
2023-06-22 11:22:42 +0545

    deleted

--- webpack.mix.js
+++
@@ -1,9 +0,0 @@
-// webpack.mix.js
-
-let mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js/app.js')
-.sass('resources/scss/app.scss', 'public/css/app.css')
-.setPublicPath('public');
-
-
